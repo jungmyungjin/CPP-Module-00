fb6fc90195f56249f04a8a8067829ecb4bba069a
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2021-11-03 21:28:09 +0900

    ex01 클래스 리펙토링

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,71 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjung <mjung@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 21:06:41 by mjung             #+#    #+#             */
+/*   Updated: 2021/11/03 21:20:15 by mjung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-// #include <cstring>
 #include <string>
-#include "PhoneBook.h"
+#include "PhoneBook.hpp"
 
-using namespace phone_book;
-
-void show_pages(PhoneBook page[8])
+void PhoneBook::input_contact (int _idx)
 {
-	for(int i=0; i<8; i++)
-	{
-		page[i].print_page();
-	}
+    _idx = _idx;
+
+    std::cout << "First name : ";
+    std::getline(std::cin, first_name_);
+    std::cout << "Last name : ";
+    std::getline(std::cin, last_name_);
+    std::cout << "Nick name : ";
+    std::getline(std::cin, nick_name_);
+    std::cout << "Phone number : ";
+    std::getline(std::cin, phone_number_);
+    std::cout << "darkest secret... : ";
+    std::getline(std::cin, darkest_secret_);
 }
 
-void show_privew(PhoneBook page[8])
+void PhoneBook::print_page (void) const
 {
-	std::cout << std::right << std::setw(10) << "Index";
-	std::cout << "|";
-	std::cout << std::right << std::setw(10) << "First Name";
-	std::cout << "|";
-	std::cout << std::right << std::setw(10) << "Last Name";
-	std::cout << "|";
-	std::cout << std::right << std::setw(10) << "Nick Name";
-	std::cout << "|";
-	std::cout << std::right << std::setw(10) << "Cell Phone";
-	std::cout << "|";
-	std::cout << std::right << std::setw(10) << "Secret";
-	std::cout << std::endl;
-	
-	for(int i=0; i<8; i++)
-	{
-		page[i].print_privew(i);
-	}
+    std::cout << "First name : " << first_name_ << std::endl;
+    std::cout << "Last name : " << last_name_ << std::endl;
+    std::cout << "Nick name : " << nick_name_ << std::endl;
+    std::cout << "Phone number : " << phone_number_ << std::endl;
+    std::cout << "Darkest secret : " << darkest_secret_ << std::endl;
 }
 
-int main(void)
+void PhoneBook::print_column(std::string _text) const
 {
-	std::string command;
-	PhoneBook page[8];
-	int idx = 0;
-	
-	while(std::getline(std::cin, command))
-	{
-		if (command.compare("exit") == 0)
-		{
-			exit(0);
-		}
-		else if (command.compare("add") == 0)
-		{
-			idx %= 8;
-			page[idx].add_contact(idx);
-			idx++;
-		}
-		else if (command.compare("search") == 0)
-		{
-			int select_index;
-			std::string input_index;
-			std::stringstream ss;
-			show_privew(page);
-			std::cout << "Select index : ";
-			std::getline(std::cin, input_index);
-			ss << input_index;
-			ss >> select_index;
-			if (0 <= select_index && select_index <= 8 && ss.fail() == false)
-				page[select_index].print_page();
-			else
-				std::cout << "This is an unauthorized number." << std::endl;
-		}
-	}
+    // std::sew = 너비 설정
+    // std::right = 오른쪽 정렬
+    if (_text.length() <= 10)
+        std::cout << std::right << std::setw(10) << _text;
+    else
+    {
+        _text =  _text.substr(0,9) + ".";
+        std::cout << std::right << std::setw(10) << _text;
+    }
+}
+void PhoneBook::print_privew(int _index) const
+{
+    print_column(std::to_string(_index));
+    std::cout << "|";
+    print_column(first_name_);
+    std::cout << "|";
+    print_column(last_name_);
+    std::cout << "|";
+    print_column(nick_name_);
+    std::cout << "|";
+    print_column(phone_number_);
+    std::cout << "|";
+    print_column(darkest_secret_);
+    std::cout << std::endl;
 }
